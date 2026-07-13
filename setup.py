274821b9e43f809ef11e967797700dc863ca187f
Yash Sharma <iyashsharmahere@gmail.com>
2026-07-13 12:41:16 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages , setup
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requiremnets
+    '''
+    requirements=[]
+    with open('requirements.txt') as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
+setup(
+    name='Machine Learning Project',
+    version= '0.0.1',
+    author= 'Yash Sharma',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
+
+
